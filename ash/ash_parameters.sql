cdb9db0fcb37132232feca6203e35446c79b4551
pauljstuart <paulstuart@mail.com>
2019-07-01 13:54:28 +0100

    Create ash_parameters.sql

---
+++ ash/ash_parameters.sql
@@ -0,0 +1,46 @@
+
+_ash_eflush_trigger
+ 
+66
+ 
+The percentage above which if the in-memory ASH is full the emergency flusher will be triggered
+ 
+
+
+_ash_enable
+ 
+TRUE
+ 
+To enable or disable Active Session sampling and flushing
+ 
+
+
+_ash_min_mmnl_dump
+ 
+90
+ 
+Minimum Time interval passed to consider MMNL Dump
+ 
+
+
+_ash_sample_all
+ 
+FALSE
+ 
+To enable or disable sampling every connected session including ones waiting for idle waits
+ 
+
+
+_ash_sampling_interval
+ 
+1000
+ 
+Time interval between two successive Active Session samples in millisecs
+ 
+
+
+_ash_size
+ 
+1048618
+ 
+To set the size of the in-memory Active Session History buffers
