af22a5b9d4f0a143a52f9fd92f180ed3c2539ee6
pauljstuart <paulstuart@mail.com>
2019-07-05 14:42:00 +0100

    Update SQLdeveloper.js

--- vba/SQLdeveloper.js
+++ vba/SQLdeveloper.js
@@ -1,4 +1,3 @@
-
 // Start SQL Developer
 // 
 // PJS
@@ -8,10 +7,10 @@ var mbInformation = 64;
 var mbCancel = 2;
 
 
-var sSQLDeveloper = 'C:\\Users\\stuartp\\Documents\\programs\\SQLdeveloper41';
-var sOracleHome = "C:\\Users\\stuartp\\Documents\\programs\\instantclient_12_1";
+var sSQLDeveloper = 'C:\\Users\\stuartp\\Documents\\programs\\SQLdeveloper_19_1';
+var sOracleHome = "C:\\Users\\stuartp\\Documents\\programs\\instantclient_18_5";
 var sTNSAdmin = "P:\\Oracle\\Network\\Admin";
-var sIDE = "c:\\users\\stuartp\\documents\\programs\\SQLdeveloper41_files2";
+var sIDE = "c:\\users\\stuartp\\documents\\programs\\SQLdeveloper18_files";
 var sJavaHome = sSQLDeveloper + "\\jdk";
 
 var WshShell = WScript.CreateObject("WScript.Shell");
@@ -19,19 +18,18 @@ var WshShell = WScript.CreateObject("WScript.Shell");
 var WshSystemEnv = WshShell.Environment( "Process");
 
 var sText = "SQL Developer = " + sSQLDeveloper +	  "\n" +
-             "Oracle Home = " + sOracleHome + "\n" +
+              "Oracle Home = " + sOracleHome + "\n" +
              "TNS Admin = " + sTNSAdmin + "\n" +
              "Java Home = " + sJavaHome + "\n" +
              "IDE Home = " + sIDE;
 
 WScript.Echo( sText );
 
-WshSystemEnv( "PATH") = sSQLDeveloper + "\\bin;" + sOracleHome + ";" + sJavaHome + "\\jre\\bin";
+WshSystemEnv( "PATH") =  sOracleHome + ";" 
 WshSystemEnv( "ORACLE_HOME" )= sOracleHome;
 WshSystemEnv( "TNS_ADMIN") = sTNSAdmin;
 WshSystemEnv( "IDE_USER_DIR") = sIDE;
 
 WshShell.Run(sSQLDeveloper + "\\sqldeveloper.exe", 1, 0	);
-
 
 
