196b6100792f0c6726790c21e413514166a2506f
pauljstuart <paulstuart@mail.com>
2019-07-05 11:10:26 +0100

    Create robocopy_options.sql

---
+++ pl-sql/robocopy_options.sql
@@ -0,0 +1,10 @@
+
+download :
+
+
+C:\Windows\System32\Robocopy.exe p:\documents\programs c:\users\stuartp\documents\programs /s /e /nfl /mt
+
+upload :
+
+C:\Windows\System32\Robocopy.exe c:\users\stuartp\documents\programs p:\documents\programs /s /e /purge /nfl /mt
+
